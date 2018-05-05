3e4cffd8e0501e3ac144267bc9d005367bc6fb6f
metzschs <metzschs@oregonstate.edu>
2018-05-05 03:26:19 -0700

    Added part of search function to return information about Where a State ships freight given a freight code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-d3.csv("./data/commodity_types.csv", function (error, data) {
+d3.csv("./data/commodity_types.csv", function (error1, com_data) {
     var select = d3.select("body")
         .append("div")
         .append("select")
@@ -10,7 +10,7 @@ d3.csv("./data/commodity_types.csv", function (error, data) {
         });
 
     select.selectAll("option")
-        .data(data)
+        .data(com_data)
         .enter()
         .append("option")
         .attr("value", function (d) {
@@ -25,4 +25,18 @@ d3.csv("./data/commodity_types.csv", function (error, data) {
 
 function dispCommData(d) {
     console.log(d);
+    var commodity = d;
+
+    d3.csv("./data/faf_commodity_data_dom_only.csv", function(faf_data) {
+        var freight_totals = d3.nest()
+            .key(function(d) { 
+                if ( d.dms_orig != d.dms_dest ) {
+                    return d.dms_orig; 
+                }})
+            .key(function(d) { return d.sctg2; })
+            .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
+            .entries(faf_data);
+        console.log(JSON.stringify(freight_totals));
+    });
+        
 }
