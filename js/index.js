39549ac1e480860a4823b1adaa30340add8bcf6b
metzschs <metzschs@oregonstate.edu>
2018-05-05 18:25:09 -0700

    Added call to pull out import values for each state

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ function dispCommData(d) {
     d3.csv("./data/faf_commodity_data_dom_only.csv", function(faf_data) {
         // Pulls out sum weight of specific good from State given commodity code
         // Data returned in freight_totals in a list with indexes of {dms_orig, sum(tons_2015)}
-        var freight_totals = d3.nest()
+        var export_totals = d3.nest()
             .key(function(d) { 
                 // Validate that origin and destination are not the same and commodity code matches
                 if ( d.dms_orig != d.dms_dest && d.sctg2 == commodity) {
@@ -38,7 +38,17 @@ function dispCommData(d) {
             // Sums up tons_2015 for a specific good in a specific origin state
             .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
             .entries(faf_data);
-        console.log(freight_totals);
+        console.log(export_totals);
+        var import_totals = d3.nest()
+            .key(function(d) { 
+                // Validate that origin and destination are not the same and commodity code matches
+                if ( d.dms_orig != d.dms_dest && d.sctg2 == commodity) {
+                    return d.dms_dest;
+                }})
+            // Sums up tons_2015 for a specific good in a specific destination state
+            .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
+            .entries(faf_data);
+        console.log(import_totals);
     });
         
 }
