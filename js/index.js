cb2e8b58d72b2752b2c78e3c91abaeb1896e720b
metzschs <metzschs@oregonstate.edu>
2018-05-05 19:22:49 -0700

    Possible working graph, but is not visible at the moment

--- js/index.js
+++ js/index.js
@@ -26,6 +26,32 @@ function dispCommData(d) {
     console.log(d);
     var commodity = d;
 
+    var margin = {top: 10, right: 0, bottom: 0, left: 0},
+        width = 400 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+        .range([0, width]);
+
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([0, height], 0.1);
+
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickSize(0)
+        .tickPadding(6);
+
+    var svg = d3.select("data_div").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     d3.csv("./data/faf_commodity_data_dom_only.csv", function(faf_data) {
         // Pulls out sum weight of specific good from State given commodity code
         // Data returned in freight_totals in a list with indexes of {dms_orig, sum(tons_2015)}
@@ -46,9 +72,35 @@ function dispCommData(d) {
                     return d.dms_dest;
                 }})
             // Sums up tons_2015 for a specific good in a specific destination state
-            .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
+            .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015 * -1; }); })
             .entries(faf_data);
         console.log(import_totals);
+
+        x.domain([d3.min(import_totals, function(d) {return d.values}), d3.max(export_totals, function(d) {return d.values})]);
+        if(Object.keys(export_totals).length > Object.keys(import_totals).length){
+            y.domain(export_totals.map(function(d) { return export_totals.key; }));
+        } else {
+            y.domain(import_totals.map(function(d) { return export_totals.key; }));
+        }
+
+        svg.selectAll(".bar")
+            .data(faf_data)
+        .enter().append("rect")
+            .attr("class", function(d) { return "bar bar--" + (export_totals.value < 0 ? "negative" : "positive"); })
+            .attr("x", function(d) { return x(Math.min(0, export_totals.value)); })
+            .attr("y", function(d) { return y(export_totals.key); })
+            .attr("width", function(d) { return Math.abs(x(export_totals.value) - x(0)); })
+            .attr("height", y.rangeBand());
+
+      svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+      svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + x(0) + ",0)")
+            .call(yAxis);
     });
         
 }
