cfa6f2a24fa2965ba8efa4fa316dc22f7fe2b15c
metzschs <metzschs@oregonstate.edu>
2018-05-05 23:55:43 -0700

    Updated chart to print out in color, still need to fix issue of negative values not starting at 0

--- js/index.js
+++ js/index.js
@@ -30,64 +30,84 @@ function dispCommData(d) {
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .range([0, width]);
+    var x = d3.scale.linear()
+        .range([0, width]);
 
-var y = d3.scale.ordinal()
-    .rangeRoundBands([0, height], 0.1);
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([0, height], 0.1);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickSize(0)
-    .tickPadding(6);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickSize(0)
+        .tickPadding(6);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("../data/faf_commodity_data_dom_only.csv", function(error, data) {
-  var export_totals = d3.nest()
-      .key(function(d) { 
-          // Validate that origin and destination are not the same and commodity code matches
-          // Commodity is hardcoded for testing purposes
-          if ( d.dms_orig != d.dms_dest && d.sctg2 == commodity) {
-              return d.dms_orig;
-          }})
-      // Sums up tons_2015 for a specific good in a specific origin state
-      .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
-      .entries(data);
-  export_totals.pop();
-  console.log(export_totals);
+    d3.csv("../data/faf_commodity_data_dom_only.csv", function(error, data) {
+        var export_totals = d3.nest()
+            .key(function(d) { 
+                // Validate that origin and destination are not the same and commodity code matches
+                // Commodity is hardcoded for testing purposes
+                if ( d.dms_orig != d.dms_dest && d.sctg2 == commodity) {
+                    return d.dms_orig;
+                }
+            })
+            // Sums up tons_2015 for a specific good in a specific origin state
+            .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
+            .entries(data);
+        export_totals.pop();
+        console.log(export_totals);
+        var import_totals
+        d3.csv("../data/faf_commodity_data_dom_only.csv", function(error, data) {
+            var import_totals = d3.nest()
+                .key(function(d) { 
+                    // Validate that origin and destination are not the same and commodity code matches
+                    // Commodity is hardcoded for testing purposes
+                    if ( d.dms_orig != d.dms_dest && d.sctg2 == commodity) {
+                        return d.dms_dest;
+                    }
+                })
+                // Sums up tons_2015 for a specific good in a specific origin state
+                .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015 * -1; }); })
+                .entries(data);
+            import_totals.pop();
+            console.log(import_totals);
 
-  x.domain(d3.extent(export_totals, function(d) { return Math.max(d.values); })).nice();
-  y.domain(data.map(function(d) { return d.dms_orig; }));
+            x.domain([d3.min(import_totals, function(d) { return d.values; }), d3.max(export_totals, function(d) { return d.values; })]).nice();
+            y.domain(data.map(function(d) { return d.dms_orig; }));
+            console.log(d3.min(import_totals, function(d) { return d.values; }));
+            console.log(d3.max(export_totals, function(d) { return d.values; }));
 
-  svg.selectAll(".bar")
-      .data(export_totals)
-      .enter()
-      .append("rect")
-      .attr("class", function(d) { return "bar bar--" + (d.values < 0 ? "negative" : "positive"); })
-      .attr("x", function(d) { return x(Math.min(0, d.values)); })
-      .attr("y", function(d) { return y(d.key); })
-      .attr("width", function(d) { return Math.abs(x(d.values)); })
-      .attr("height", y.rangeBand());
+            var combined = export_totals.concat(import_totals);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+            svg.selectAll(".bar")
+                .data(combined)
+                .enter()
+                .append("rect")
+                .attr("class", function(d) { return "bar bar_" + (d.values < 0 ? "negative" : "positive"); })
+                .attr("x", function(d) { return x(Math.min(0, d.values)); })
+                .attr("y", function(d) { return y(d.key); })
+                .attr("width", function(d) { return Math.abs(x(d.values)); })
+                .attr("height", y.rangeBand());
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + x(0) + ",0)")
-      .call(yAxis);
-});
-        
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + x(0) + ",0)")
+                .call(yAxis);
+        });
+    });
 }
