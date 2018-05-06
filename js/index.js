bed205f41b86f6f4df8601c70e8c9bb7c6faf551
metzschs <metzschs@oregonstate.edu>
2018-05-05 22:41:58 -0700

    WORKING HORIZONTAL HISTOGRAM!!!!!!!!!!!!!!!

--- js/index.js
+++ js/index.js
@@ -53,62 +53,41 @@ function dispCommData(d) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.csv("./data/faf_commodity_data_dom_only.csv", function(faf_data) {
-        // Pulls out sum weight of specific good from State given commodity code
-        // Data returned in freight_totals in a list with indexes of {dms_orig, sum(tons_2015)}
-        var export_totals = d3.nest()
-            .key(function(d) { 
-                // Validate that origin and destination are not the same and commodity code matches
-                if ( d.dms_orig != d.dms_dest && d.sctg2 == commodity) {
-                    return d.dms_orig;
-                }})
-            // Sums up tons_2015 for a specific good in a specific origin state
-            .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
-            .entries(faf_data);
-        export_totals.pop();
-        console.log(export_totals);
-        var import_totals = d3.nest()
-            .key(function(d) { 
-                // Validate that origin and destination are not the same and commodity code matches
-                if ( d.dms_orig != d.dms_dest && d.sctg2 == commodity) {
-                    return d.dms_dest;
-                }})
-            // Sums up tons_2015 for a specific good in a specific destination state
-            .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015 * -1; }); })
-            .entries(faf_data);
-        import_totals.pop();
-        console.log(import_totals);
+    d3.csv("test.csv", function(error, data) {
+      x.domain(d3.extent(data, function(d) { return d.tons_2015; })).nice();
+      y.domain(data.map(function(d) { return d.dms_orig; }));
 
-        
+      var export_totals = d3.nest()
+          .key(function(d) { 
+              // Validate that origin and destination are not the same and commodity code matches
+              if ( d.dms_orig != d.dms_dest && d.sctg2 == 1) {
+                  return d.dms_orig;
+              }})
+          // Sums up tons_2015 for a specific good in a specific origin state
+          .rollup(function(v) { return d3.sum(v, function(d) { return d.tons_2015; }); })
+          .entries(data);
+      export_totals.pop();
+      console.log(export_totals);
 
-        x.domain([
-            d3.min(import_totals, function(d) {return d.values}),
-            d3.max(export_totals, function(d) {return d.values})
-        ]);
-        if(Object.keys(export_totals).length > Object.keys(import_totals).length){
-            y.domain(export_totals.map(function(d) { return export_totals.key; }));
-        } else {
-            y.domain(import_totals.map(function(d) { return export_totals.key; }));
-        }
-
-        svg.selectAll(".bar")
-            .data(export_totals)
-            .enter().append("rect")
-            .attr("class", function(d) { return "bar bar_" + (export_totals.values < 0 ? "negative" : "positive"); })
-            .attr("x", function(d) { return x(d.values); })
-            .attr("y", function(d) { return y(d.key); })
-            .attr("width", function(d) { return Math.abs(x(d.values) - x(0)); })
-            .attr("height", y.rangeBand() / Object.keys(export_totals).length);
+      svg.selectAll(".bar")
+          .data(export_totals)
+          .enter()
+          .append("rect")
+          .attr("class", function(d) { return "bar bar--" + (d.values < 0 ? "negative" : "positive"); })
+          .attr("x", function(d) { return x(Math.min(0, d.values)); })
+          .attr("y", function(d) { return y(d.key); })
+          .attr("width", function(d) { return Math.abs(x(d.values) - x(0)); })
+          .attr("height", y.rangeBand());
 
       svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
 
       svg.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + x(0) + ",0)")
-            .call(yAxis);
+          .attr("class", "y axis")
+          .attr("transform", "translate(" + x(0) + ",0)")
+          .call(yAxis);
     });
         
 }
